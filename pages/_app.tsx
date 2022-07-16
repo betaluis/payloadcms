205b78711f732d25da89f9034f9118cd6c619bf2
betaluis <luis.betancourt.dev@gmail.com>
2022-07-16 16:04:25 +0000

    installed and set up jss-react.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 
 import '../css/style.css';
 
-const MyApp = ({ Component, pageProps }: AppProps): React.ReactElement => (
-  <Component {...pageProps} />
-);
+const MyApp = ({ Component, pageProps }: AppProps): React.ReactElement => {
+  useEffect(() => {
+
+    const style = document.getElementById('server-side-styles');
+
+    if (style) {
+      style.parentNode.removeChild(style);
+    }
+
+  }, [])
+
+  return <Component {...pageProps} />;
+};
 
 export default MyApp;
